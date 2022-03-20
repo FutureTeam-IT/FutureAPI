150ab2ba0ba9137144925790d8f1f4322dd458ff
Daniele Guglietti <gugliettidaniele@gmail.com>
2022-03-21 00:03:35 +0100

    📚 docs(FileManager): added documentation.

--- src/main/java/it/futurecraft/api/files/FileManager.java
+++ src/main/java/it/futurecraft/api/files/FileManager.java
@@ -12,6 +12,14 @@ import java.util.Map;
 public class FileManager {
     private static Map<String, PluginFile<?>> files = new HashMap<>();
 
+    /**
+     * Load a new TOML file.
+     * @param path The path to the file directory.
+     * @param name The file name.
+     * @param model The class object of the file model.
+     * @param <T> The file model type.
+     * @return The PluginFile instance of the file.
+     */
     public static <T> PluginFile<T> toml(@NotNull Path path, @NotNull String name, @NotNull Class<T> model) {
         if (files.containsKey(name)) {
             return (PluginFile<T>) files.get(name);
@@ -26,6 +34,13 @@ public class FileManager {
         return file;
     }
 
+    /**
+     * Create a file object.
+     * @param path The path to the file directory.
+     * @param name The file name.
+     * @param extension The file extension.
+     * @return The file object.
+     */
     public static File toJavaFile(@NotNull Path path, @NotNull String name, @NotNull String extension) {
         return new File(path.toFile(), name + "." + extension);
     }
