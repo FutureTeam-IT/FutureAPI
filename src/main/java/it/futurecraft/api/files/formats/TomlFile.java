e9b170c818b883dbe6edac5811624402a696da8a
Daniele Guglietti <gugliettidaniele@gmail.com>
2022-03-21 21:47:53 +0100

    ✨ feat(PluginFile): made `PluginFile` an abstract class and implemented default logic.

--- src/main/java/it/futurecraft/api/files/formats/TomlFile.java
+++ src/main/java/it/futurecraft/api/files/formats/TomlFile.java
@@ -15,49 +15,29 @@ import java.nio.file.Path;
  * @param <T> The model representing the file content.
  * @since v0.1.1
  */
-public class TomlFile<T> implements PluginFile<T> {
+public final class TomlFile<T> extends PluginFile<T> {
     /**
      * The file write for toml format.
      */
     public static final TomlWriter WRITER = new TomlWriter();
-    private final Class<T> model;
-    private final Path path;
-    private final String name;
-    private T data;
 
     public TomlFile(Path path, String name, Class<T> model, T data) {
-        this.data = data;
-        this.model = model;
-        this.path = path;
-        this.name = name;
+        super(path, name, model, data);
     }
 
     @Override
     public void save() throws IOException {
-        WRITER.write(data, new File(path.toFile(), name + ".toml"));
+        WRITER.write(getData(), FileManager.toJavaFile(getPath(), getName(), "toml"));
     }
 
     @Override
     public T reload() {
-        data = new Toml().read(FileManager.toJavaFile(path, name, "toml"))
+        T data = new Toml().read(FileManager.toJavaFile(getPath(), getName(), "toml"))
                 .to(model);
 
+        setData(data);
+
         return data;
-    }
-
-    @Override
-    public T getData() {
-        return data;
-    }
-
-    @Override
-    public Path getPath() {
-        return path;
-    }
-
-    @Override
-    public String getName() {
-        return name;
     }
 
     @Override
