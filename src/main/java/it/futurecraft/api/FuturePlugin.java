9a10b5b5a08b6e9bd5ff63e3336eb52ee447adee
Daniele Guglietti <gugliettidaniele@gmail.com>
2022-03-21 18:21:17 +0100

    📚 docs(FuturePlugin): added more details to methods documentation.

--- src/main/java/it/futurecraft/api/FuturePlugin.java
+++ src/main/java/it/futurecraft/api/FuturePlugin.java
@@ -6,11 +6,15 @@ package it.futurecraft.api;
 public interface FuturePlugin {
     /**
      * Initialize the plugin.
+     * <p>
+     * It's the first method called on server startup.
      */
     void init();
 
     /**
      * Destroy the plugin.
+     * <p>
+     * It's the first method called on server shutdown.
      */
     void destroy();
 }
