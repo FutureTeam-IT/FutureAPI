3fd2740c12f85eea7ded72b68d4abb43cbbc4278
Daniele Guglietti <gugliettidaniele@gmail.com>
2022-03-25 00:11:16 +0100

    ✨ feat: added `Cancellable` interface.

---
+++ src/main/java/it/futurecraft/api/event/Cancellable.java
@@ -0,0 +1,19 @@
+package it.futurecraft.api.event;
+
+/**
+ * A cancellable event.
+ *
+ * @since v0.2.0
+ */
+public interface Cancellable {
+    /**
+     * Cancel the event.
+     */
+    void cancel();
+
+    /**
+     * Check whether the event has been cancelled or not.
+     * @return <code>true</code> If the event has been cancelled.
+     */
+    boolean isCancelled();
+}
