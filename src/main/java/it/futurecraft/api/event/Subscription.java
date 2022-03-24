948de969e033cb932fbadbe75e001c272e505c24
Daniele Guglietti <gugliettidaniele@gmail.com>
2022-03-25 00:23:42 +0100

    ✨ feat: added getter to `Subscription`

--- src/main/java/it/futurecraft/api/event/Subscription.java
+++ src/main/java/it/futurecraft/api/event/Subscription.java
@@ -1,5 +1,7 @@
 package it.futurecraft.api.event;
 
+import java.util.function.Consumer;
+
 /**
  * Represent a subscription to event dispatch.
  *
@@ -7,6 +9,22 @@ package it.futurecraft.api.event;
  * @since v0.2.0
  */
 public interface Subscription<E extends FutureEvent> {
+    /**
+     * Get the event handler from the subscription.
+     *
+     * @return The consumer object.
+     * @since v0.2.1
+     */
+    Consumer<E> getHandler();
+
+    /**
+     * Get the event class from the subscription.
+     *
+     * @return The event class.
+     * @since v0.2.1
+     */
+    Class<E> getEvent();
+
     /**
      * Unsubscribe from the event listening.
      */
