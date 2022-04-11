7678e66717122d5ce3d33cdcc80385298fdd85ca
Daniele Guglietti <gugliettidaniele@gmail.com>
2022-04-11 18:07:44 +0200

    ⚙️ build: moved to a multi module project.

---
+++ future-api/build.gradle.kts
@@ -0,0 +1,11 @@
+repositories {
+    maven("https://libraries.minecraft.net")
+}
+
+dependencies {
+    implementation("net.kyori:adventure-api:4.10.1")
+    compileOnly("com.mojang:brigadier:1.0.18")
+    implementation("com.moandjiezana.toml:toml4j:0.7.2")
+    compileOnly("mysql:mysql-connector-java:8.0.28")
+    implementation("com.zaxxer:HikariCP:5.0.1")
+}
