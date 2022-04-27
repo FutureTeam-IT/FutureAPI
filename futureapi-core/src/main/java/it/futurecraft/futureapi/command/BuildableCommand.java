ad41a46ff96653be67b4e748c25cb5d632668595
Daniele Guglietti <gugliettidaniele@gmail.com>
2022-04-27 23:21:59 +0200

    ✨ feat: added an interface for commands buildable to brigadier

---
+++ futureapi-core/src/main/java/it/futurecraft/futureapi/command/BuildableCommand.java
@@ -0,0 +1,36 @@
+
+/*
+ * futureapi Copyright (C) 2022 FutureTeam-IT
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package it.futurecraft.futureapi.command;
+
+import com.mojang.brigadier.builder.ArgumentBuilder;
+
+/**
+ * A command which is buildable to Mojang's Brigadier.
+ *
+ * @param <S> The command sender.
+ * @param <T> The command type.
+ */
+public interface BuildableCommand<S, T extends ArgumentBuilder<S, T>> {
+    /**
+     * Create the builder for the command.
+     *
+     * @return The command builder.
+     */
+    T toBuilder();
+}
