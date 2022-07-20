7d1d9bb932c11cc691717784266b62602d431bed
Daniele Guglietti <gugliettidaniele@gmail.com>
2022-07-20 19:57:17 +0200

    ✨ feat: added default value for type reference.

--- futureapi-core/src/main/java/it/futurecraft/futureapi/util/TypeReference.java
+++ futureapi-core/src/main/java/it/futurecraft/futureapi/util/TypeReference.java
@@ -18,8 +18,9 @@
 
 package it.futurecraft.futureapi.util;
 
+import java.lang.reflect.Array;
 import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
+import java.util.*;
 
 /**
  * Base class used to obtain full generic type information by subclassing.
@@ -27,16 +28,77 @@ import java.lang.reflect.Type;
  * @param <T> The type of the subclass.
  */
 public abstract class TypeReference<T> {
-    private final Type type;
+    private final Class<T> type;
 
     protected TypeReference() {
-        Type superClass = getClass().getGenericSuperclass();
-
-        if (superClass instanceof Class<?>) {
+        if (getClass().getGenericSuperclass() instanceof Class<?>) {
             throw new IllegalArgumentException("Internal error: TypeReference constructed without actual type information.");
         }
 
-        this.type = ((ParameterizedType) superClass).getActualTypeArguments()[0];
+        ParameterizedType generic = (ParameterizedType) getClass().getGenericSuperclass();
+        this.type = (Class<T>) generic.getActualTypeArguments()[0];
+    }
+
+    /**
+     * Get the default value of the specified type.
+     *
+     * @param type The type.
+     * @return The default value.
+     * @throws Exception If an error occurs.
+     */
+    public static Object getDefaultValue(Class<?> type) throws Exception {
+        if (type == Boolean.class) {
+            return false;
+        }
+        if (type == Character.class) {
+            return (char) 0;
+        }
+        if (type == Byte.class) {
+            return (byte) 0;
+        }
+        if (type == Short.class) {
+            return (short) 0;
+        }
+        if (type == Integer.class) {
+            return 0;
+        }
+        if (type == Long.class) {
+            return 0L;
+        }
+        if (type == Float.class) {
+            return 0F;
+        }
+        if (type == Double.class) {
+            return 0D;
+        }
+        if (type == String.class) {
+            return "";
+        }
+        if (type == Set.class) {
+            return new HashSet<>();
+        }
+        if (type == List.class) {
+            return new ArrayList<>();
+        }
+        if (type == Map.class) {
+            return new LinkedHashMap<>();
+        }
+        if (type == Queue.class || type == Deque.class) {
+            return new ArrayDeque<>();
+        }
+        if (type == SortedSet.class || type == NavigableSet.class) {
+            return new TreeSet<>();
+        }
+        if (type == SortedMap.class || type == NavigableMap.class) {
+            return new TreeMap<>();
+        }
+        if (type.isEnum()) {
+            return type.getEnumConstants()[0];
+        }
+        if (type.isArray()) {
+            return Array.newInstance(type.getComponentType(), 0);
+        }
+        return type.getConstructor().newInstance();
     }
 
     /**
@@ -44,7 +106,17 @@ public abstract class TypeReference<T> {
      *
      * @return The type.
      */
-    public Type getType() {
+    public Class<T> getType() {
         return type;
+    }
+
+    /**
+     * Get the default value of the referred type.
+     *
+     * @return The default value.
+     * @throws Exception If an error occurs.
+     */
+    public T getDefaultValue() throws Exception {
+        return (T) getDefaultValue(type);
     }
 }
